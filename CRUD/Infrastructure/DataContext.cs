3b01cc2cef9b0df93164cfe3defef9224402d933
Hai Nguyen <nvminh09@gmail.com>
2025-05-08 15:35:57 +0700

    update

--- CRUD/Infrastructure/DataContext.cs
+++ CRUD/Infrastructure/DataContext.cs
@@ -1,9 +1,11 @@
-﻿using CRUD.Models;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using CRUD.Models;
 
 namespace CRUD.Infrastructure
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
+
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {        
---
+++ CRUD/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class User
+    {
+        public string? Id { get; set; }
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 2.")]
+        [Display(Name = "Username")]
+        public string? UserName { get; set; }
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+        [DataType(DataType.Password), Required, MinLength(4, ErrorMessage = "Minimum length is 2.")]
+        public string? Password { get; set; }
+    }
+}
